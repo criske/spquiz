872febcc2390d633994512ea152bec1665f6e908
cristianpela <cristypela05@gmail.com>
2021-07-12 09:37:26 +0300

    unified success handler.
    also redirects always to GET "/user" on formLogin.

--- src/main/java/pcf/crksdev/spquiz/SpquizSecurity.java
+++ src/main/java/pcf/crksdev/spquiz/SpquizSecurity.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import pcf.crksdev.spquiz.services.user.SpQuizUserDetailsService;
 import pcf.crksdev.spquiz.services.user.SpquizOAuth2UserService;
 import pcf.crksdev.spquiz.services.user.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +29,25 @@ class SpquizSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
+
+    private final AuthenticationSuccessHandler successHandler = (
+        request,
+        response, authentication
+    ) -> {
+        class MutatedMethodHttpServletRequest extends HttpServletRequestWrapper {
+            public MutatedMethodHttpServletRequest(HttpServletRequest request) {
+                super(request);
+            }
+
+            @Override
+            public String getMethod() {
+                return HttpMethod.GET.name();
+            }
+        }
+        request.getRequestDispatcher("/user")
+            .forward(new MutatedMethodHttpServletRequest(request), response);
+    };
 
     @Override
     public void configure(WebSecurity web) {
@@ -48,11 +72,11 @@ class SpquizSecurity extends WebSecurityConfigurerAdapter {
             .logoutSuccessUrl("/")
             .and()
             .formLogin()
-            .successForwardUrl("/user")
+            .successHandler(successHandler)
             .loginPage("/")
             .and()
             .oauth2Login()
-            .defaultSuccessUrl("/user")
+            .successHandler(successHandler)
             .userInfoEndpoint(config -> config
                 .oidcUserService(new SpquizOAuth2UserService(environment))
             );
@@ -61,6 +85,7 @@ class SpquizSecurity extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
+
 }
--- src/main/java/pcf/crksdev/spquiz/controllers/Dashboard.java
+++ src/main/java/pcf/crksdev/spquiz/controllers/Dashboard.java
@@ -1,11 +1,9 @@
 package pcf.crksdev.spquiz.controllers;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import pcf.crksdev.spquiz.services.quiz.QuizService;
 import pcf.crksdev.spquiz.services.user.SpquizUser;
 
@@ -18,22 +16,11 @@ public final class Dashboard {
         this.quizService = quizService;
     }
 
-    @PostMapping ("/user")
-    public String userRedirected(Model model, @AuthenticationPrincipal SpquizUser principal) {
-        return getUser(model, principal);
-    }
-
     @GetMapping("/user")
     public String user(Model model, @AuthenticationPrincipal SpquizUser principal) {
-        return getUser(model, principal);
-    }
-
-    @NotNull
-    private String getUser(Model model,
-                           SpquizUser principal) {
-        model.addAttribute("user", principal);
         var question = quizService.startOrResumeSession().orElseThrow();
         model.addAttribute("question", question);
+        model.addAttribute("user", principal);
         return "dashboard";
     }
 
